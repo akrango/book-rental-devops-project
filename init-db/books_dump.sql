93e6f55e94f63641e0547f9d4c02fd6ff89ebb65
akrango <angelakrango27@gmail.com>
2024-06-24 04:26:24 +0200

    fixing docker-compose errors

---
+++ init-db/books_dump.sql
@@ -0,0 +1,196 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.3 (Debian 16.3-1.pgdg120+1)
+-- Dumped by pg_dump version 16.3 (Ubuntu 16.3-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: author; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.author (
+    id bigint NOT NULL,
+    name character varying(255),
+    surname character varying(255),
+    country_id bigint
+);
+
+
+ALTER TABLE public.author OWNER TO postgres;
+
+--
+-- Name: author_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.author_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.author_seq OWNER TO postgres;
+
+--
+-- Name: book; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.book (
+    id bigint NOT NULL,
+    available_copies integer,
+    category character varying(255),
+    name character varying(255),
+    author_id bigint,
+    CONSTRAINT book_category_check CHECK (((category)::text = ANY ((ARRAY['NOVEL'::character varying, 'THRILLER'::character varying, 'HISTORY'::character varying, 'FANTASY'::character varying, 'BIOGRAPHY'::character varying, 'CLASSICS'::character varying, 'DRAMA'::character varying])::text[])))
+);
+
+
+ALTER TABLE public.book OWNER TO postgres;
+
+--
+-- Name: book_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.book_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.book_seq OWNER TO postgres;
+
+--
+-- Name: country; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.country (
+    id bigint NOT NULL,
+    continent character varying(255),
+    name character varying(255)
+);
+
+
+ALTER TABLE public.country OWNER TO postgres;
+
+--
+-- Name: country_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.country_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.country_seq OWNER TO postgres;
+
+--
+-- Data for Name: author; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.author (id, name, surname, country_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: book; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.book (id, available_copies, category, name, author_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: country; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.country (id, continent, name) FROM stdin;
+\.
+
+
+--
+-- Name: author_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.author_seq', 1, false);
+
+
+--
+-- Name: book_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.book_seq', 1, false);
+
+
+--
+-- Name: country_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.country_seq', 1, false);
+
+
+--
+-- Name: author author_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.author
+    ADD CONSTRAINT author_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: book book_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.book
+    ADD CONSTRAINT book_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: country country_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.country
+    ADD CONSTRAINT country_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: book fkklnrv3weler2ftkweewlky958; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.book
+    ADD CONSTRAINT fkklnrv3weler2ftkweewlky958 FOREIGN KEY (author_id) REFERENCES public.author(id);
+
+
+--
+-- Name: author fkq9nw1djh062h5omgdo26u0gjf; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.author
+    ADD CONSTRAINT fkq9nw1djh062h5omgdo26u0gjf FOREIGN KEY (country_id) REFERENCES public.country(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
